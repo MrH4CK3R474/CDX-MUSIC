30f87e4258acadd8c6c001b7bae07c07edc136fb
MrH4CK3R474 <148775562+MrH4CK3R474@users.noreply.github.com>
2024-02-07 16:24:48 +0530

    Update left.py

--- DAXXMUSIC/plugins/tools/left.py
+++ DAXXMUSIC/plugins/tools/left.py
@@ -92,7 +92,7 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
             )
         
             caption = f"**#New_Member_ADDED**\n\n**๏** {user.mention} **ʜᴀs ʟᴇғᴛ ᴛʜɪs ɢʀᴏᴜᴘ**\n**๏ sᴇᴇ ʏᴏᴜ sᴏᴏɴ ᴀɢᴀɪɴ..!**"
-            button_text = "๏ ᴠɪᴇᴡ ᴜsᴇʀ ๏"
+            button_text = "{mention}"
 
             # Generate a deep link to open the user's profile
             deep_link = f"tg://openmessage?user_id={user.id}"
